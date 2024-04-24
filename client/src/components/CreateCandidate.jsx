5ecab66849a5922000baca471070afadb1e8cb2a
Nooby9 <1330762153@qq.com>
2024-04-23 21:41:03 -0700

    Fixed bug

--- client/src/components/CreateCandidate.jsx
+++ client/src/components/CreateCandidate.jsx
@@ -9,69 +9,69 @@ function CreateCandidate() {
   const [newType, setNewType] = useState('');
   const { getAccessTokenSilently } = useAuth0();
 
-  useEffect(() => {
-    async function fetchCandidateTypes() {
-      try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/candidate-types`);
-        const data = await response.json();
-        setCandidateTypes(data);
-        setSelectedType('none');
-        // if (data.length > 0) {
-        //   setSelectedType(data[0].id); // Default to the first type
-        // }
-      } catch (error) {
-        console.error('Error fetching candidate types:', error);
-      }
-    }
 
+  const fetchCandidateTypes = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/candidate-types`);
+      const data = await response.json();
+      setCandidateTypes(data);
+      setSelectedType(data.length > 0 ? data[0].id : 'none');
+    } catch (error) {
+      console.error('Error fetching candidate types:', error);
+    }
+  };
+
+  useEffect(() => {
     fetchCandidateTypes();
   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    let typeId = selectedType;
+    let typeId = selectedType === 'none' ? undefined : selectedType;
     const accessToken = await getAccessTokenSilently();
+
     if (newType && selectedType === 'none') {
-        try {
-          const typeResponse = await fetch(`${process.env.REACT_APP_API_URL}/candidate-types`, {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            },
-            body: JSON.stringify({ type: newType })
-          });
-          if (!typeResponse.ok) {
-            if (typeResponse.status === 409) {
-              const errorMessage = await typeResponse.text();
-              alert(errorMessage);  
-              return;  
-            }
-            throw new Error('Failed to create new type');
-          }
-          const newTypeData = await typeResponse.json();
-          typeId = newTypeData.id;  
-        } catch (error) {
-          alert(`Error creating new type: ${error.message}`);
-          return;
-        }
-    }
-    try {
-        const candidateResponse = await fetch(`${process.env.REACT_APP_API_URL}/candidates`, {
+      try {
+        const typeResponse = await fetch(`${process.env.REACT_APP_API_URL}/candidate-types`, {
           method: 'POST',
-          headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${accessToken}` },
-          body: JSON.stringify({ name: candidateName, typeId })
+          headers: { 
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`
+          },
+          body: JSON.stringify({ type: newType })
         });
-        if (!candidateResponse.ok) {
-          throw new Error('Failed to create candidate');
+        if (!typeResponse.ok) {
+          const errorMessage = await typeResponse.text();
+          alert(errorMessage);  
+          return;  
         }
-        alert('Candidate created successfully!');
-        setCandidateName('');
-        setNewType('');
-        setSelectedType('');
+        const newTypeData = await typeResponse.json();
+        typeId = newTypeData.id;
+        await fetchCandidateTypes(); 
       } catch (error) {
-        alert(`Error: ${error.message}`);
+        alert(`Error creating new type: ${error.message}`);
+        return;
       }
+    }
+
+
+    try {
+      const candidateResponse = await fetch(`${process.env.REACT_APP_API_URL}/candidates`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${accessToken}` },
+        body: JSON.stringify({ name: candidateName, typeId })
+      });
+      if (!candidateResponse.ok) {
+        throw new Error('Failed to create candidate');
+      }
+      alert('Candidate created successfully!');
+      setCandidateName('');
+      setNewType('');
+      setSelectedType('none'); 
+      await fetchCandidateTypes(); 
+    } catch (error) {
+      alert(`Error: ${error.message}`);
+    }
   };
 
   return (
@@ -94,7 +94,7 @@ function CreateCandidate() {
       </label>
       <label>
         Or Define a New Type:
-        <input type="text" value={newType} onChange={(e) => setNewType(e.target.value)} disabled={selectedType !== 'none'} />
+        <input type="text" value={newType} disabled={selectedType !== 'none'} onChange={(e) => setNewType(e.target.value)} />
       </label>
       <button type="submit">Submit</button>
     </form>
